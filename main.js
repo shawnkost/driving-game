4199054306e73fda7dc85a7a25064e7f9ea21063
Shawn Kost <shawnmkost@gmail.com>
2020-12-11 13:29:20 -0800

    Updated the event listener to change the direction of the image class, and the direction in our data model. #3

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
 
-var $car = document.querySelector("img");
+var $car = document.querySelector('.car');
+var carData = {
+  direction: 'east'
+};
 
+document.addEventListener('keydown', function (event) {
 
-document.addEventListener("keydown", function() {
+  if (event.key === 'ArrowUp') {
+    carData.direction = 'north';
+    $car.setAttribute('class', 'car, north');
+  } else if (event.key === 'ArrowRight') {
+    carData.direction = 'east';
+    $car.setAttribute('class', 'car');
+  } else if (event.key === 'ArrowDown') {
+    carData.direction = 'south';
+    $car.setAttribute('class', 'car south');
+  } else if (event.key === 'ArrowLeft') {
+    carData.direction = 'west';
+    $car.setAttribute('class', 'car, west');
+  }
 
-
-})
+});
