c1acb26d843de73ec7941261940e3f11161b64be
Shawn Kost <shawnmkost@gmail.com>
2020-12-11 14:53:24 -0800

    Updated the event listener function from feedback. #3

--- main.js
+++ main.js
@@ -8,16 +8,12 @@ document.addEventListener('keydown', function (event) {
 
   if (event.key === 'ArrowUp') {
     carData.direction = 'north';
-    $car.setAttribute('class', 'car north');
   } else if (event.key === 'ArrowRight') {
     carData.direction = 'east';
-    $car.setAttribute('class', 'car');
   } else if (event.key === 'ArrowDown') {
     carData.direction = 'south';
-    $car.setAttribute('class', 'car south');
   } else if (event.key === 'ArrowLeft') {
     carData.direction = 'west';
-    $car.setAttribute('class', 'car west');
   }
-
+  $car.setAttribute('class', 'car ' + carData.direction);
 });
